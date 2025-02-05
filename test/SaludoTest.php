48da965a86abe9a493a7607bf7604d364141facd
Daniel Alos Prats <daalpr@floridauniversitaria.es>
2025-02-05 19:52:42 +0100

    4 commit

--- test/SaludoTest.php
+++ test/SaludoTest.php
@@ -1,23 +1,28 @@
 <?php
 use PHPUnit\Framework\TestCase;
 use HolaMundoMultilenguaje\Saludo;
+
 class SaludoTest extends TestCase
 {
- public function testGenerarSaludoAleatorio()
- {
- $saludo = new Saludo();
- $resultado = $saludo->generarSaludoAleatorio();
+    public function testGenerarSaludoAleatorio()
+    {
+        $saludo = new Saludo();
+        $resultado = $saludo->generarSaludoAleatorio();
 
- // Verifica que el resultado es un string
- $this->assertIsString($resultado['saludo']);
+        // Verifica que el resultado es un array y tiene las claves correctas
+        $this->assertArrayHasKey('saludo', $resultado);
+        $this->assertArrayHasKey('codigo', $resultado);
 
- // Verifica que el resultado contiene un saludo válido
- $this->assertNotEmpty($resultado['saludo']);
+        // Verifica que el resultado es un string
+        $this->assertIsString($resultado['saludo']);
 
- // Verifica que el código también es un string
- $this->assertIsString($resultado['codigo']);
+        // Verifica que el resultado contiene un saludo válido
+        $this->assertNotEmpty($resultado['saludo']);
 
- // Verifica que el código no esté vacío
- $this->assertNotEmpty($resultado['codigo']);
- }
+        // Verifica que el código también es un string
+        $this->assertIsString($resultado['codigo']);
+
+        // Verifica que el código no esté vacío
+        $this->assertNotEmpty($resultado['codigo']);
+    }
 }
