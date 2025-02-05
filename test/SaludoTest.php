576d2b855fd3490de457c515d18cb6372fd06f6e
Daniel Alos Prats <daalpr@floridauniversitaria.es>
2025-02-05 20:11:20 +0100

    10 commit

--- test/SaludoTest.php
+++ test/SaludoTest.php
@@ -14,6 +14,7 @@ class SaludoTest extends TestCase
         $this->assertArrayHasKey('codigo', $resultado);
 
         // Verifica que el resultado es un string
+        
         $this->assertIsString($resultado['saludo']);
 
         // Verifica que el resultado contiene un saludo válido
